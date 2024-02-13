021722a938c35683548bf7c536237e4822839302
MailBIT Sales <yuvalleberstein21@gmail.com>
2024-02-13 11:38:14 +0200

    updated12

--- server/server.js
+++ server/server.js
@@ -15,19 +15,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// const allowedOrigins = ['https://mybooks-eight.vercel.app/'];
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     optionsSuccessStatus: 200,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// };
-// app.use(cors(corsOptions));
+const allowedOrigins = ['https://mybooks-eight.vercel.app/'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+app.use(cors(corsOptions));
 
 // Handle OPTIONS requests for all routes
 app.options('*', (req, res) => {
